8f06588ff06c39a745b4bcdf8cef2444325da02b
Dmytro Krekota <dmytro.krekota@gmail.com>
2019-07-07 20:19:25 +0300

    New version: update jlto, code style

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-let stream = require('stream')
-let jlto = require('jlto')
+let stream = require('stream');
+let jlto = require('jlto');
 
-module.exports = function (options) {
+module.exports = (options) => {
   let transform = new stream.Transform({
     objectMode: true
-  })
+  });
 
-  transform._transform = function (file, encoding, callback) {
+  transform._transform = (file, encoding, callback) => {
     if (file.isNull()) {
-      return callback(null, file)
+      return callback(null, file);
     }
-    let source = String(file.contents)
-    try {
-      let result = jlto.optimizeString(source, options)
-      file.contents = new Buffer(result)
-    } catch (ignored) {}
-    callback(null, file)
-  }
+    let source = String(file.contents);
 
-  return transform
-}
+    try {
+      let result = jlto.optimizeString(source, options);
+
+      file.contents = new Buffer(result);
+    } catch (ignored) {}
+    callback(null, file);
+  };
+
+  return transform;
+};
